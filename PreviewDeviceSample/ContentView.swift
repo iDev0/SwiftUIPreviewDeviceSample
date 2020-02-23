9d636acfc8ed42e38484c36d01c7cd306fc9260c
SamplesMaker <insert@kakao.com>
2020-02-23 17:29:12 +0900

    SwiftUI Preview Devices Sample

--- PreviewDeviceSample/ContentView.swift
+++ PreviewDeviceSample/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("다중 디바이스 테스트")
+                .font(.largeTitle)
+            
+            Rectangle()
+                .foregroundColor(.blue)
+                .padding(.horizontal, 10)
+            
+            Text("테스팅").font(.title)
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        Group {
+            ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+            .previewDisplayName("iPhone 11 Pro Max")
+            
+            ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+            .previewDisplayName("iPhone SE")
+        }
+        
+        
+        
     }
 }
